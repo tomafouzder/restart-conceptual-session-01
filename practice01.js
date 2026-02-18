7aa903fef743e591a27df52e5c33552d65a4ff63
tomafouzder <tomafouzder12@gmail.com>
2026-02-18 16:18:13 +0600

    added this switch case basic concept

--- practice01.js
+++ practice01.js
@@ -141,24 +141,82 @@ const marks = 93;
 //     console.log("Fail this exam");
 // }
 
-if (marks < 33) {
-    console.log("F");
-} else if (marks >= 33 && marks <= 39) {
-    console.log("D");
-} else if (marks >= 40 && marks <= 49) {
-    console.log("C");
-} else if (marks >= 50 && marks <= 59) {
-    console.log("B");
-} else if (marks >= 60 && marks <= 69) {
-    console.log("-A");
-} else if (marks >= 60 && marks <= 79) {
-    console.log("A");
-} else if (marks >= 80 && marks <= 89) {
-    console.log("A+");
-} else {
-    console.log("Golden A+");
-}
+// if (marks < 33) {
+//     console.log("F");
+// } else if (marks >= 33 && marks <= 39) {
+//     console.log("D");
+// } else if (marks >= 40 && marks <= 49) {
+//     console.log("C");
+// } else if (marks >= 50 && marks <= 59) {
+//     console.log("B");
+// } else if (marks >= 60 && marks <= 69) {
+//     console.log("-A");
+// } else if (marks >= 60 && marks <= 79) {
+//     console.log("A");
+// } else if (marks >= 80 && marks <= 89) {
+//     console.log("A+");
+// } else {
+//     console.log("Golden A+");
+// }
 
+
+//Switch case:
+
+const color = "Green";
+
+//This switch case is wrong:
+// switch (color) {
+//     case "Red":
+//         console.log("OK, This is Red");
+//     case "Green":
+//         console.log("OK, This is Green");
+//     case "Blue":
+//         console.log("OK, This is Blue");
+//     default:
+//         console.log("Invalid");
+// };
+
+
+// This switch case is right:
+// switch (color) {
+//     case "Red":
+//         console.log("OK, This is Red");
+//         break;
+//     case "Green":
+//         console.log("OK, This is Green");
+//         break;
+//     case "Blue":
+//         console.log("OK, This is Blue");
+//         break;
+//     default:
+//         console.log("Invalid");
+// };
+
+
+
+const role = {
+    admin: "admin",
+    user: "user",
+    moderator: "moderator"
+}
+let page;
+
+switch (role.HeroAlom) {
+    case role.user:
+        page = "Please, permit me, I am a user of this page";
+        break;
+    case role.moderator:
+        page = "Please, permit me, I am a moderator of this page";
+        break;
+    case role.admin:
+        page = "I am Admin in this page";
+        break;
+
+    default:
+        page = "Not permitted for you ";
+        break;
+}
+console.log(page);
 
 
 
